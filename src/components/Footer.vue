d43989a98e96d9c96cb6d7ebe8448543dafa8fca
Apfirebolt <aspper20@gmail.com>
2023-11-01 06:25:56 +0530

    feat: layout changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
       <div
         class="border-t border-gray-200 py-8 text-sm text-gray-500 text-center sm:text-left"
       >
-        <span class="block sm:inline">&copy; 2021 Tailwind Labs Inc.</span>
+        <span class="block sm:inline">&copy; 2023 PokeAPI Vue3 Inc. </span>
         <span class="block sm:inline">All rights reserved.</span>
       </div>
     </div>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,5 @@
 <template>
-    <div class="min-h-full">
-      
+    <div class="min-h-full">  
       <main class="-mt-24 pb-8">
         <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
           <h1 class="sr-only">Profile</h1>
@@ -19,15 +18,11 @@
                           <img class="mx-auto h-20 w-20 rounded-full" :src="user.imageUrl" alt="" />
                         </div>
                         <div class="mt-4 text-center sm:mt-0 sm:pt-1 sm:text-left">
-                          <p class="text-sm font-medium text-gray-600">Welcome back,</p>
-                          <p class="text-xl font-bold text-gray-900 sm:text-2xl">{{ user.name }}</p>
-                          <p class="text-sm font-medium text-gray-600">{{ user.role }}</p>
+                          <p class="text-xl font-bold text-gray-900 sm:text-2xl">Pokemon</p>
+                          <p class="text-sm font-medium text-gray-600">
+                            List of all pokemon coming from the API
+                          </p>
                         </div>
-                      </div>
-                      <div class="mt-5 flex justify-center sm:mt-0">
-                        <a href="#" class="flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">
-                          View profile
-                        </a>
                       </div>
                     </div>
                   </div>
@@ -40,44 +35,23 @@
                   </div>
                 </div>
               </section>
-  
-              <!-- Actions panel -->
-              <section aria-labelledby="quick-links-title">
-                <div class="rounded-lg bg-gray-200 overflow-hidden shadow divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px">
-                  <h2 class="sr-only" id="quick-links-title">Quick links</h2>
-                  <div v-for="(action, actionIdx) in actions" :key="action.name" :class="[actionIdx === 0 ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none' : '', actionIdx === 1 ? 'sm:rounded-tr-lg' : '', actionIdx === actions.length - 2 ? 'sm:rounded-bl-lg' : '', actionIdx === actions.length - 1 ? 'rounded-bl-lg rounded-br-lg sm:rounded-bl-none' : '', 'relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-cyan-500']">
-                    <div>
-                      <span :class="[action.iconBackground, action.iconForeground, 'rounded-lg inline-flex p-3 ring-4 ring-white']">
-                        <component :is="action.icon" class="h-6 w-6" aria-hidden="true" />
-                      </span>
+            </div>
+
+            <div class="grid grid-cols-1 gap-4">
+              <!-- Welcome panel -->
+              <section aria-labelledby="profile-overview-title">
+                <div class="rounded-lg bg-white overflow-hidden shadow">
+                  <h2 class="sr-only" id="profile-overview-title">Profile Overview</h2>
+                  
+                  <div class="border-t border-gray-200 bg-gray-50 grid grid-cols-1 divide-y divide-gray-200 sm:grid-cols-3 sm:divide-y-0 sm:divide-x">
+                    <div v-for="stat in stats" :key="stat.label" class="px-6 py-5 text-sm font-medium text-center">
+                        <span class="text-gray-900">{{ stat.value }}</span>
+                        {{ ' ' }}
+                        <span class="text-gray-600">{{ stat.label }}</span>
                     </div>
-                    <div class="mt-8">
-                      <h3 class="text-lg font-medium">
-                        <a :href="action.href" class="focus:outline-none">
-                          <!-- Extend touch target to entire panel -->
-                          <span class="absolute inset-0" aria-hidden="true" />
-                          {{ action.name }}
-                        </a>
-                      </h3>
-                      <p class="mt-2 text-sm text-gray-500">
-                        Doloribus dolores nostrum quia qui natus officia quod et dolorem. Sit repellendus qui ut at blanditiis et quo et molestiae.
-                      </p>
-                    </div>
-                    <span class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
-                      <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                        <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-                      </svg>
-                    </span>
                   </div>
                 </div>
               </section>
-            </div>
-  
-            <!-- Right column -->
-            <div class="grid grid-cols-1 gap-4 bg-gray-100 shadow rounded p-6">
-                <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae, dicta.
-                </p>
             </div>
           </div>
         </div>
@@ -86,30 +60,6 @@
   </template>
   
   <script>
-  import {
-    Menu,
-    MenuButton,
-    MenuItem,
-    MenuItems,
-    Popover,
-    PopoverButton,
-    PopoverOverlay,
-    PopoverPanel,
-    TransitionChild,
-    TransitionRoot,
-  } from '@headlessui/vue'
-  import {
-    AcademicCapIcon,
-    BadgeCheckIcon,
-    BellIcon,
-    CashIcon,
-    ClockIcon,
-    MenuIcon,
-    ReceiptRefundIcon,
-    UsersIcon,
-    XIcon,
-  } from '@heroicons/vue/outline'
-  import { SearchIcon } from '@heroicons/vue/solid'
   
   const user = {
     name: 'Chelsea Hagon',
@@ -135,65 +85,15 @@
     { label: 'Sick days left', value: 4 },
     { label: 'Personal days left', value: 2 },
   ]
-  const actions = [
-    {
-      icon: ClockIcon,
-      name: 'Request time off',
-      href: '#',
-      iconForeground: 'text-teal-700',
-      iconBackground: 'bg-teal-50',
-    },
-    {
-      icon: BadgeCheckIcon,
-      name: 'Benefits',
-      href: '#',
-      iconForeground: 'text-purple-700',
-      iconBackground: 'bg-purple-50',
-    },
-    {
-      icon: UsersIcon,
-      name: 'Schedule a one-on-one',
-      href: '#',
-      iconForeground: 'text-sky-700',
-      iconBackground: 'bg-sky-50',
-    },
-    { icon: CashIcon, name: 'Payroll', href: '#', iconForeground: 'text-yellow-700', iconBackground: 'bg-yellow-50' },
-    {
-      icon: ReceiptRefundIcon,
-      name: 'Submit an expense',
-      href: '#',
-      iconForeground: 'text-rose-700',
-      iconBackground: 'bg-rose-50',
-    },
-    {
-      icon: AcademicCapIcon,
-      name: 'Training',
-      href: '#',
-      iconForeground: 'text-indigo-700',
-      iconBackground: 'bg-indigo-50',
-    },
-  ]
+
   
   export default {
-    components: {
-      Menu,
-      MenuButton,
-      MenuItem,
-      MenuItems,
-      Popover,
-      PopoverButton,
-      PopoverOverlay,
-      PopoverPanel,
-      TransitionChild,
-      TransitionRoot,
-    },
     setup() {
       return {
         user,
         navigation,
         userNavigation,
         stats,
-        actions,
       }
     },
   }
